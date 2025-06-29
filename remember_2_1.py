df60189fed7f99055e99c0c3916b7d186471e051
iZelikov <ivan@zelikov.name>
2025-06-29 03:55:54 +0300

    refactor: add type hinting

--- remember_2_1.py
+++ remember_2_1.py
@@ -3,10 +3,9 @@ def hide_card(raw_card_number: str) -> str:
     return '*' * 12 + ''.join(card_number[12:])
 
 
-def same_parity(numbers):
+def same_parity(numbers: list) -> list:
     return [i for i in numbers if i % 2 == numbers[0] % 2]
 
-def is_valid(string):
-    return string.isdigit() and len(string) in [4,5,6]
 
-print(is_valid(''))
+def is_valid(string: str) -> bool:
+    return string.isdigit() and len(string) in [4, 5, 6]
