b1c35ce0f46f5fd4280e2f403f751dc25e74a583
iZelikov <ivan@zelikov.name>
2025-06-29 03:33:59 +0300

    feat: test()

---
+++ alpha_code.py
@@ -0,0 +1,4 @@
+# from test import *
+#
+# test(2310066)
+
---
+++ remember_2_1.py
@@ -0,0 +1,4 @@
+def hide_card(raw_card_number: str) -> str:
+    card_number = [i for i in raw_card_number if i != ' ']
+    return '*' * 12 + ''.join(card_number[12:])
+
